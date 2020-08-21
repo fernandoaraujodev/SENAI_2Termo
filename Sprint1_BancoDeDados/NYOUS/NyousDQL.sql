7452b99512d398b3c5f038de608c1b03d325d9fa
Fernando Araujo <fernandovcaraujo@gmail.com>
2020-08-21 11:51:42 -0300

    DQL do projeto Nyous finalizada

---
+++ Sprint1_BancoDeDados/NYOUS/NyousDQL.sql
@@ -0,0 +1,15 @@
+USE Nyous;
+GO
+
+SELECT * FROM Categoria;
+
+SELECT * FROM Localizacao;
+
+SELECT Usuario.Nome, Usuario.Email, Acesso.Tipo AS PermissaoAcesso FROM Usuario
+	INNER JOIN Acesso ON Usuario.IdAcesso = Acesso.IdAcesso;
+
+SELECT * FROM Evento
+	INNER JOIN Categoria ON Evento.IdCategoria = Categoria.IdCategoria
+	INNER JOIN Localizacao ON Evento.IdLocalizacao = Localizacao.IdLocalizacao
+
+SELECT * FROM Usuario;
