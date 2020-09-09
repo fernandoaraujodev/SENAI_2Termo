55c2edbe0bb46fe18e47c28b3a082d275046cb3f
Fernando Araujo <fernandovcaraujo@gmail.com>
2020-09-08 22:28:58 -0300

    Erro ao executar o comando Add-Migration InitialCreate

---
+++ Sprint2_BackEnd-API/EntityFramework/Contexts/PedidoContext.cs
@@ -0,0 +1,25 @@
+﻿using EntityFramework.Domains;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Contexts
+{
+    public class PedidoContext : DbContext
+    {
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<PedidoItem> PedidosItens { get; set; }
+
+        // sobreescrevendo para conectar ao SQL Server, sem precisar instanciar 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-DA6MBAT\SQLEXPRESS;Initial Catalog=DbSenai;User ID=sa;Password=ps132");
+                
+                base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Sprint2_BackEnd-API/EntityFramework/Domains/Pedido.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Domains
+{
+    public class Pedido
+    {
+        /// <summary>
+        /// Define a classe Produto
+        /// </summary>
+        [Key]
+        public Guid Id { get; set; }
+        public string Status { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public Pedido()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
