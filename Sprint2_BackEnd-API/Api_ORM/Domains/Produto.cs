69216412f4e8c89e5215ffaea0676be89dc5c0d7
Fernando Araujo <fernandovcaraujo@gmail.com>
2020-09-09 17:28:25 -0300

    Add-Migration InitialCreate realizado com sucesso

---
+++ Sprint2_BackEnd-API/Api_ORM/Domains/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_ORM.Domains
+{
+    public class Produto
+    {
+        /// <summary>
+        /// Define a classe Produto
+        /// </summary>
+        [Key]
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+        public Produto()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
